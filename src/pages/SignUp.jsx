43d50d7a9f20c78b31d2416684ffd2163391ec66
Gal Barak <gal.barak2510@gmail.com>
2023-04-29 17:42:42 +0300

    create sign in + sign up + forgot password pages

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,123 @@
+import { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = formData;
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  function toggleIcon() {
+    setShowPassword((prevState) => !prevState);
+  }
+
   return (
-    <div>SignUp</div>
-  )
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign Up</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            className="w-full rounded-2xl"
+            src="https://images.unsplash.com/photo-1633265486064-086b219458ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="key"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white
+           border-gray-300 rounded transition ease-in-out mb-6"
+              type="text"
+              id="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Full name"
+            />
+            <input
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white
+           border-gray-300 rounded transition ease-in-out mb-6"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+            />
+            <div className="relative mb-6">
+              <input
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white
+              border-gray-300 rounded transition ease-in-out"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+              />
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  className="absolute right-3 top-3 text-xl
+              cursor-pointer"
+                  onClick={toggleIcon}
+                />
+              ) : (
+                <AiFillEye
+                  className="absolute right-3 top-3 text-xl
+                cursor-pointer"
+                  onClick={toggleIcon}
+                />
+              )}
+            </div>
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">
+                Have an account?
+                <Link
+                  to="/sign-in"
+                  className="text-red-600 hover:text-red-800 transition duration-100 ease-in-out ml-1"
+                >
+                  Sign in
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/forgot-password"
+                  className="text-blue-600 hover:text-blue-800 transition duration-100 ease-in-out"
+                >
+                  Forgot password?
+                </Link>
+              </p>
+            </div>
+            <button
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm 
+          font-medium uppercase rounded shadow-md hover:bg-blue-700 transition 
+          duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+              type="submit"
+            >
+              Sign in
+            </button>
+            <div className=" my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <button
+              className="w-full bg-red-700 text-white px7 py-3 text-sm uppercase
+              font-medium rounded shadow-md hover:bg-red-800 hover:shadow-lg
+              transition duration-150 ease-in-out active:shadow-lg active:bg-red-900"
+            >
+              Continue with google
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
